ea30364c2e6f2d14a9a89cc951a561f73e6929b9
Smahesh26 <sunainamahesh1@gmail.com>
2024-09-19 22:02:06 +0530

    refund1

--- src/components/common/social-icon/SocialIcon.jsx
+++ src/components/common/social-icon/SocialIcon.jsx
@@ -1,28 +1,5 @@
 import Link from 'next/link';
 import React from 'react';
-<<<<<<< HEAD
-
-const SocialIcon = ({socialWrapperClass}) => {
-    return (
-        <div className={socialWrapperClass}>
-        <Link href="https://www.facebook.com/profile.php?id=61553561020127" target="_blank">
-          <i className="fa-brands fa-facebook-f"></i>
-        </Link>
-        <Link href="https://twitter.com/" target="_blank">
-          <i className="fa-brands fa-twitter"></i>
-        </Link>
-        <Link href="https://www.linkedin.com/" target="_blank">
-          <i className="fa-brands fa-instagram"></i>
-        </Link>
-        <Link href="https://www.pinterest.com/" target="_blank">
-          <i className="fa-brands fa-pinterest-p"></i>
-        </Link>
-      </div>
-    );
-};
-
-export default SocialIcon;
-=======
 import PropTypes from 'prop-types'; // Import PropTypes for validation
 
 const SocialIcon = ({ socialWrapperClass }) => {
@@ -50,4 +27,3 @@ SocialIcon.propTypes = {
 };
 
 export default SocialIcon;
->>>>>>> master
